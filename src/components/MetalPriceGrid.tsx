d7edbcfdf0d7f458008447fff3dd40d90201f47e
Burak Süloğlu <bs@192.168.1.9>
2025-11-28 00:19:38 +0300

    Fix MetalPriceGrid metalId type

--- src/components/MetalPriceGrid.tsx
+++ src/components/MetalPriceGrid.tsx
@@ -113,7 +113,7 @@ export default function MetalPriceGrid({ lang = "en" }: MetalPriceGridProps) {
           return (
             <MetalPriceCard
               key={metal.id}
-              metalId={metal.id}
+              metalId={metal.symbol}
               symbol={metal.symbol}
               name={metalName}
               pricePerGram={priceData}
